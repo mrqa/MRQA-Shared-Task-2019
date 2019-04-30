d46df6036f311d13c2465bacf7be896a5c9d7c28
Adam Fisch <adamfisch15@gmail.com>
2019-04-30 00:44:58 -0400

    update readme & paths

--- download_in_domain_dev.sh
+++ download_in_domain_dev.sh
@@ -6,10 +6,10 @@ OUTPUT=$1
 
 mkdir -p $OUTPUT
 
-wget https://s3.us-east-2.amazonaws.com/mrqa/data/dev/SQuAD.jsonl.gz -O $OUTPUT/SQuAD.jsonl.gz
-wget https://s3.us-east-2.amazonaws.com/mrqa/data/dev/NewsQA.jsonl.gz -O $OUTPUT/NewsQA.jsonl.gz
-wget https://s3.us-east-2.amazonaws.com/mrqa/data/dev/TriviaQA-unfilt.jsonl.gz -O $OUTPUT/TriviaQA-unfilt.jsonl.gz
-wget https://s3.us-east-2.amazonaws.com/mrqa/data/dev/TriviaQA-web.jsonl.gz -O $OUTPUT/TriviaQA-web.jsonl.gz
-wget https://s3.us-east-2.amazonaws.com/mrqa/data/dev/SearchQA.jsonl.gz -O $OUTPUT/SearchQA.jsonl.gz
-wget https://s3.us-east-2.amazonaws.com/mrqa/data/dev/HotpotQA.jsonl.gz -O $OUTPUT/HotpotQA.jsonl.gz
-wget https://s3.us-east-2.amazonaws.com/mrqa/data/dev/NaturalQuestionsShort.jsonl.gz -O $OUTPUT/NaturalQuestions.jsonl.gz
+wget https://s3.us-east-2.amazonaws.com/mrqa/release/dev/SQuAD.jsonl.gz -O $OUTPUT/SQuAD.jsonl.gz
+wget https://s3.us-east-2.amazonaws.com/mrqa/release/dev/NewsQA.jsonl.gz -O $OUTPUT/NewsQA.jsonl.gz
+wget https://s3.us-east-2.amazonaws.com/mrqa/release/dev/TriviaQA-unfilt.jsonl.gz -O $OUTPUT/TriviaQA-unfilt.jsonl.gz
+wget https://s3.us-east-2.amazonaws.com/mrqa/release/dev/TriviaQA-web.jsonl.gz -O $OUTPUT/TriviaQA-web.jsonl.gz
+wget https://s3.us-east-2.amazonaws.com/mrqa/release/dev/SearchQA.jsonl.gz -O $OUTPUT/SearchQA.jsonl.gz
+wget https://s3.us-east-2.amazonaws.com/mrqa/release/dev/HotpotQA.jsonl.gz -O $OUTPUT/HotpotQA.jsonl.gz
+wget https://s3.us-east-2.amazonaws.com/mrqa/release/dev/NaturalQuestionsShort.jsonl.gz -O $OUTPUT/NaturalQuestions.jsonl.gz
--- download_train.sh
+++ download_train.sh
@@ -6,10 +6,10 @@ OUTPUT=$1
 
 mkdir -p $OUTPUT
 
-wget https://s3.us-east-2.amazonaws.com/mrqa/data/train/SQuAD.jsonl.gz -O $OUTPUT/SQuAD.jsonl.gz
-wget https://s3.us-east-2.amazonaws.com/mrqa/data/train/NewsQA.jsonl.gz -O $OUTPUT/NewsQA.jsonl.gz
-wget https://s3.us-east-2.amazonaws.com/mrqa/data/train/TriviaQA-unfilt.jsonl.gz -O $OUTPUT/TriviaQA-unfilt.jsonl.gz
-wget https://s3.us-east-2.amazonaws.com/mrqa/data/train/TriviaQA-web.jsonl.gz -O $OUTPUT/TriviaQA-web.jsonl.gz
-wget https://s3.us-east-2.amazonaws.com/mrqa/data/train/SearchQA.jsonl.gz -O $OUTPUT/SearchQA.jsonl.gz
-wget https://s3.us-east-2.amazonaws.com/mrqa/data/train/HotpotQA.jsonl.gz -O $OUTPUT/HotpotQA.jsonl.gz
-wget https://s3.us-east-2.amazonaws.com/mrqa/data/train/NaturalQuestionsShort.jsonl.gz -O $OUTPUT/NaturalQuestions.jsonl.gz
+wget https://s3.us-east-2.amazonaws.com/mrqa/release/train/SQuAD.jsonl.gz -O $OUTPUT/SQuAD.jsonl.gz
+wget https://s3.us-east-2.amazonaws.com/mrqa/release/train/NewsQA.jsonl.gz -O $OUTPUT/NewsQA.jsonl.gz
+wget https://s3.us-east-2.amazonaws.com/mrqa/release/train/TriviaQA-unfilt.jsonl.gz -O $OUTPUT/TriviaQA-unfilt.jsonl.gz
+wget https://s3.us-east-2.amazonaws.com/mrqa/release/train/TriviaQA-web.jsonl.gz -O $OUTPUT/TriviaQA-web.jsonl.gz
+wget https://s3.us-east-2.amazonaws.com/mrqa/release/train/SearchQA.jsonl.gz -O $OUTPUT/SearchQA.jsonl.gz
+wget https://s3.us-east-2.amazonaws.com/mrqa/release/train/HotpotQA.jsonl.gz -O $OUTPUT/HotpotQA.jsonl.gz
+wget https://s3.us-east-2.amazonaws.com/mrqa/release/train/NaturalQuestionsShort.jsonl.gz -O $OUTPUT/NaturalQuestions.jsonl.gz
