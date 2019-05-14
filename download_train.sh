c203db19f28b31ade307c6bd055c04d6e566442a
Adam Fisch <adamfisch15@gmail.com>
2019-05-13 21:25:06 -0400

    Update download_train.sh

--- download_train.sh
+++ download_train.sh
@@ -8,7 +8,6 @@ mkdir -p $OUTPUT
 
 wget https://s3.us-east-2.amazonaws.com/mrqa/release/train/SQuAD.jsonl.gz -O $OUTPUT/SQuAD.jsonl.gz
 wget https://s3.us-east-2.amazonaws.com/mrqa/release/train/NewsQA.jsonl.gz -O $OUTPUT/NewsQA.jsonl.gz
-wget https://s3.us-east-2.amazonaws.com/mrqa/release/train/TriviaQA-unfilt.jsonl.gz -O $OUTPUT/TriviaQA-unfilt.jsonl.gz
 wget https://s3.us-east-2.amazonaws.com/mrqa/release/train/TriviaQA-web.jsonl.gz -O $OUTPUT/TriviaQA-web.jsonl.gz
 wget https://s3.us-east-2.amazonaws.com/mrqa/release/train/SearchQA.jsonl.gz -O $OUTPUT/SearchQA.jsonl.gz
 wget https://s3.us-east-2.amazonaws.com/mrqa/release/train/HotpotQA.jsonl.gz -O $OUTPUT/HotpotQA.jsonl.gz
