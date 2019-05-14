5c2646abd783f35d1de58e1da139b24c02b6d051
Adam Fisch <adamfisch15@gmail.com>
2019-05-13 21:25:35 -0400

    Update download_in_domain_dev.sh

--- download_in_domain_dev.sh
+++ download_in_domain_dev.sh
@@ -8,7 +8,6 @@ mkdir -p $OUTPUT
 
 wget https://s3.us-east-2.amazonaws.com/mrqa/release/dev/SQuAD.jsonl.gz -O $OUTPUT/SQuAD.jsonl.gz
 wget https://s3.us-east-2.amazonaws.com/mrqa/release/dev/NewsQA.jsonl.gz -O $OUTPUT/NewsQA.jsonl.gz
-wget https://s3.us-east-2.amazonaws.com/mrqa/release/dev/TriviaQA-unfilt.jsonl.gz -O $OUTPUT/TriviaQA-unfilt.jsonl.gz
 wget https://s3.us-east-2.amazonaws.com/mrqa/release/dev/TriviaQA-web.jsonl.gz -O $OUTPUT/TriviaQA-web.jsonl.gz
 wget https://s3.us-east-2.amazonaws.com/mrqa/release/dev/SearchQA.jsonl.gz -O $OUTPUT/SearchQA.jsonl.gz
 wget https://s3.us-east-2.amazonaws.com/mrqa/release/dev/HotpotQA.jsonl.gz -O $OUTPUT/HotpotQA.jsonl.gz
